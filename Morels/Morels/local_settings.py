e721fd74e83f1d43c3b46d76b5b935710a388d05
Chelsea <Chelseamegandover@gmail.com>
2015-09-28 14:52:39 -0700

    Init commit

---
+++ Morels/Morels/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Morels',
+        'USER': 'Bathulk',
+        'PASSWORD': 'rent4life99',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
