f0aa883dfd228455c47b7e0bf90b8f4a1a445b40
Chelsea <Chelseamegandover@gmail.com>
2015-10-02 11:16:54 -0700

    Finished populating db with cards

--- Morels/Game/models.py
+++ Morels/Game/models.py
@@ -4,19 +4,22 @@ from Player.models import *
 
 # Create your models here.
 
+
 class Card(models.Model):
-    cardValue = models.IntegerField(max_length=6)
-    stickValue = models.IntegerField(max_length=16)
+    cardValue = models.IntegerField()
+    stickValue = models.IntegerField()
     type = models.CharField(max_length=50)
 
     def __str__(self):
         return self.type
+
 
 class FryingPan(models.Model):
     card_id = models.ForeignKey(Card)
 
     def __str__(self):
         return self.card_id
+
 
 class Game(models.Model):
     winner = models.ForeignKey(User, default=None)
@@ -28,6 +31,7 @@ class Game(models.Model):
     forestCard = models.ManyToManyField(Card, related_name='+')
     nightDeckCard = models.ManyToManyField(Card, related_name='+')
     decayDeckCard = models.ManyToManyField(Card, related_name='+')
+
 
 class Playing_Card(models.Model):
     fryingPan_id = models.ForeignKey(FryingPan)
