dece71611a6e1e796dbf1e2176fc674e69a7e28b
Chelsea <Chelseamegandover@gmail.com>
2015-10-02 15:42:53 -0700

    Finished populating Card with all the needed cards

--- Morels/Game/views.py
+++ Morels/Game/views.py
@@ -9,8 +9,8 @@ game = Game()
 # def make_decks(request):
 #     games = Game.objects.all()
 #
-#     for i in range(len(something)):
-#
+#     for i in range():
+
 
     # Get the cards from the db
     #Loop through the cards in Cards and push them in a deck
